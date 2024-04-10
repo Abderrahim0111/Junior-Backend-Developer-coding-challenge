99dad988b1c09c167b8b5c64427b3e2dc5c81a9f
Abderrahim0111 <abderrahi.defaoui@gmail.com>
2024-04-10 16:45:31 +0000

    project init

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+
+mongoose
+  .connect(
+    "mongodb+srv://abderrahimdefaoui:PPlFRbNDyJhm5B0Y@cluster0.udyotvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => { 
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`); 
+    });  
+  })
+  .catch((err) => {
+    console.log(err);
+  });
